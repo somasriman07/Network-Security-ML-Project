ce930dff7eaa894271a6aa44b915fe48d94a5147
somasriman07 <somasriman3@gmail.com>
2026-07-07 15:22:48 +0530

    Setup.py file is ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> List[str]:
+    requirements_lst = []
+
+    try:
+        with open("requirements.txt", "r") as file:
+            requirements_lst = file.readlines()
+            requirements_lst = [req.strip() for req in requirements_lst]
+
+            if "-e ." in requirements_lst:
+                requirements_lst.remove("-e .")
+
+    except FileNotFoundError as e:
+        print(f"Requirements file not found: {e}")
+
+    return requirements_lst
+
+setup(
+    name="Network Security",
+    version="0.0.1",
+    author = "Sriman Soma",
+    author_email = "srimansoma3@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
+
